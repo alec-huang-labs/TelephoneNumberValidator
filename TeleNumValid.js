1f0dc1d5de8e5ad70e51432dcb5b0095f5055587
alec huang <67424069+alec-huang-labs@users.noreply.github.com>
2020-07-29 21:41:26 -0400

    Update TeleNumValid.js
    deleted extra code

--- TeleNumValid.js
+++ TeleNumValid.js
@@ -34,20 +34,3 @@ function telephoneCheck(str) {
 }
 console.log(telephoneCheck("2 (757) 622-7382"));
 
-
-//FCC Solution
-function telephoneCheck(str) {
-    var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
-    return regex.test(str);
-  }
-  telephoneCheck("555-555-5555");
-
-  // ^(1\s?)? chekcs for 1 at the begining if there is 1 at the begining also space following if there is so. 
-  // (\(\d{3}\)|\d{3})   followed by either brackets and three digits or just three digits
-  // [\s\-]? followed by any character in the set if they are there
-  // \d{3} followed by another three digits
-  // [\s\-]? followed by any character in the set if they are there
-  // \d{4}$ followed by 4 digits to end 
-  //Way simpler. If I had known the ? mark notation, i would have tried to implement this
-
-  
